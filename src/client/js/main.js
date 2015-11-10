084ea7c8ab117f08f1923568ced8256067acde98
amerriman <amm434@gmail.com>
2015-11-10 16:53:08 -0700

    updated route and API call

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,5 +1,53 @@
-add scripts
+// add scripts
 
-$(document).on('ready', function() {
-  console.log('sanity check!');
+// $(document).on('ready', function() {
+//   console.log('sanity check!');
+
+
+// });
+
+// function encode(text){
+//   var encoded = text.replace(/ /g, "%20");
+//   return encoded;
+// }
+
+$('#paragraph-submit').on('click', function(){
+  var text = $('#paragraph').val();
+  // console.log(text, "TEXTs");
+  //making a get request to server, and passing it the paragraph text - the data is the res.json(result) from the server side.
+  $.get('/analyze/'+ text, function(data){
+    // console.log(data[0]);
+    //make functions and call them in here with data as the parameters
+    sortSentiment(data);
+  });
+  postParagraph(text)
+});
+
+
+function sortSentiment(data){
+  var textS = $('#paragraph').val();
+  var positiveWords = [];
+  var negativeWords = [];
+  for (var i = 0; i < data.length; i++) {
+    // console.log(data[i].sentiment.type, "data sentiment type");
+    if(data[i].sentiment.type === "positive"){
+      positiveWords.push(data[i].text)
+    } else if (data[i].sentiment.type === "negative"){
+      negativeWords.push(data[i].text);
+    }
+  }
+  console.log(positiveWords, "POSITIVE WORDS");
+  console.log(negativeWords, "NEGATIVE WORDS");
+  console.log(textS, "textS");
+  //make a function to color the words, and call it here?
+  return positiveWords, negativeWords;
 }
+
+
+function postParagraph(text){
+    $('#result-paragraph').append(text);
+}
+
+
+// x[0].sentiment.type
+
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,10 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+
 });
 
+
+router.get('/analyze/:text', function(req, res){
+  // console.log(req.params, "REQ")
+  var paragraph = req.params.text;
+  // console.log(paragraph, "PARAGRAPH")
+  request({
+    method: 'POST',
+    url: 'http://gateway-a.watsonplatform.net/calls/text/TextGetRankedKeywords?text='+paragraph+'&apikey='+process.env.KEY+'&outputMode=json&sentiment=1&showSourceText=1'
+  }, function(err, response){
+    if(err){
+      console.log('err', err);
+      res.json(err);
+    } else {
+
+      var result = JSON.parse(response.body).keywords;
+      // console.log(response.body);
+      // console.log(response.body.status, "STATUS");
+      console.log(result, "RESULT");
+      res.json(result);
+      // res.render('index', {keywords: result[0].text});
+      // res.send(result);
+
+    }
+  });
+});
 
 
 
